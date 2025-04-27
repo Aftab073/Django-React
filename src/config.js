bc5a2022d7cedb431180ab640f22844dc90eb37a
Aftab073 <tamboliaftab84@gmail.com>
2025-04-27 23:58:37 +0530

    update a

--- src/config.js
+++ src/config.js
@@ -1,9 +1,5 @@
 // Base URL for API calls
-const BASE_URL = import.meta.env.VITE_BASE_URL;
-
-if (!BASE_URL) {
-  throw new Error('BASE_URL is not defined. Please check your .env file.');
-}
+const BASE_URL = import.meta.env.VITE_BASE_URL || 'https://blog-backend-hpl0.onrender.com';
 
 // API URL includes the /api path
 export const API_BASE_URL = `${BASE_URL}/api`;
